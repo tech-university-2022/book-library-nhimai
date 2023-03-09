b6ba2c31a17b3d5cca005af1e8d3084f3fdb5db3
HoangNhi0106 <maihoangnhind@gmail.com>
2023-03-09 21:59:35 +0700

    handle id

--- src/services/book.service.js
+++ src/services/book.service.js
@@ -1,12 +1,12 @@
 const prisma = require('../stores/db.store')
 
-const insertMany = async (books) => {
+const insertMany = (books) => {
 	const promises = books.map(async (book) => {
 		return prisma.book.create({
 			data: {
 				...book
 			}
-		})
+		}).catch(() => console.log(book.id))
 	})
 	Promise.all(promises)
 }
