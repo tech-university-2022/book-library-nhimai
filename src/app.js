1a68cd740d1729ff688b23f96d96af981ef6e41b
HoangNhi0106 <maihoangnhind@gmail.com>
2023-03-16 11:05:51 +0700

    update

--- src/app.js
+++ src/app.js
@@ -5,9 +5,15 @@ const router = require('./routes/index')
 const app = express()
 const PORT = 4000
 
+const corsOptions = {
+	origin:'http://localhost:3000', 
+	credentials:true,           
+	optionSuccessStatus:200
+}
+app.use(cors(corsOptions))
+
 app.use('/', router)
 app.use(express.json())
-app.use(cors())
 
 const server = app.listen(PORT, (error) => {
 	if (!error) {
